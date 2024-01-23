21858af5b36b16e2ca13748869648202b2d71410
jitendra3618 <137607500+jitendra3618@users.noreply.github.com>
2024-01-23 19:00:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var typed = new Typed('.a', {
+    strings: ["SKILLS"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    
+    loop: true
+});
+
+var typed = new Typed('.b', {
+    strings: ["PROJECTS"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    
+    loop: true
+});
+
+var typed = new Typed('.c', {
+    strings: ["EXPERIENCE"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    
+    loop: true
+});
+
+var typed = new Typed('#d', {
+    strings: ["CONTACT"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    
+    loop: true
+});
+
+var typed = new Typed('#E', {
+    strings: ["EDUCATION"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    
+    loop: true
+});
+
